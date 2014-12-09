b33714d73c52227fbeb2b95cc9b554ca1a1c3b19
kanon kitamura <kanon@kanon-no-MacBook-Pro.local>
2014-12-09 17:22:31 +0900

    Initial Commit

---
+++ MusicGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusicGame
+//
+//  Created by kanon kitamura on 2014/12/09.
+//  Copyright (c) 2014年 myname. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MusicGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MusicGame
+//
+//  Created by kanon kitamura on 2014/12/09.
+//  Copyright (c) 2014年 myname. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
